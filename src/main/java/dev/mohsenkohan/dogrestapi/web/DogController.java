356751c1061ab2f28642512f73b39075167d3f2e
Mohsen Kohan <mohsenkohan.dev@gmail.com>
2020-05-16 18:48:59 +0430

    Adding web layer

---
+++ src/main/java/dev/mohsenkohan/dogrestapi/web/DogController.java
@@ -0,0 +1,45 @@
+package dev.mohsenkohan.dogrestapi.web;
+
+import dev.mohsenkohan.dogrestapi.entity.Dog;
+import dev.mohsenkohan.dogrestapi.service.DogService;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+public class DogController {
+
+    private final DogService dogService;
+
+    public DogController(DogService dogService) {
+        this.dogService = dogService;
+    }
+
+    @GetMapping("/dogs")
+    public ResponseEntity<List<Dog>> getAllDogs() {
+        List<Dog> dogs = dogService.retrieveDogs();
+        return new ResponseEntity<>(dogs, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/breed")
+    public ResponseEntity<List<String>> getDogBreeds() {
+        List<String> breeds = dogService.retrieveDogBreeds();
+        return new ResponseEntity<>(breeds, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}/breed")
+    public ResponseEntity<String> getBreedById(@PathVariable Long id) {
+        String breed = dogService.retrieveDogBreedById(id);
+        return new ResponseEntity<>(breed, HttpStatus.OK);
+    }
+
+    @GetMapping("/dogs/name")
+    public ResponseEntity<List<String>> getDogNames() {
+        List<String> names = dogService.retrieveDogNames();
+        return new ResponseEntity<>(names, HttpStatus.OK);
+    }
+}
